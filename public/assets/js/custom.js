4c542f2a8d9fd2b621e1afdf8ed93ec29a5500ed
developement0261 <developement0261@gmail.com>
2021-08-23 15:37:16 +0530

    Designed the webpages

---
+++ public/assets/js/custom.js
@@ -0,0 +1,86 @@
+$(".VideoPopup").on('hidden.bs.modal', function (e) {
+     $(".VideoPopup iframe").attr("src",   $(".VideoPopup iframe").attr("src"));
+});
+
+
+$('#inputDate').datepicker({
+  uiLibrary: 'bootstrap4'
+});
+
+
+
+$('.client_slider_wrp').slick({
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 300,
+  autoplay: false,
+  slidesToShow: 3.2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2.2,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.services_slide').slick({
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 300,
+   autoplay: false,
+  centerPadding: '10px',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
